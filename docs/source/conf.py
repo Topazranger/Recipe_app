d5e2bbb0768c79cee5e00990bffbbdadd963e0a1
Topazranger <dillonlopes29@gmail.com>
2025-05-24 13:22:48 +0100

    Changes for code documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'y'
+project = 'Recipe_app'
 copyright = '2025, Author'
 author = 'Author'
 
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-y documentation
+Recipe App documentation
 ===============
 
 Add your content using ``reStructuredText`` syntax. See the
